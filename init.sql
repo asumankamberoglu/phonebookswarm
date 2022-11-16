3b5be049841688722e2b980dec8c0c9fa12ad0b2
asumankamberoglu <104717437+asumankamberoglu@users.noreply.github.com>
2022-11-16 15:45:34 +0300

    added

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE phonebook_db.phonebook(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    number VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO phonebook_db.phonebook (name, number)
+    VALUES
+        ("bmcgrup", "1234567890"),
+        ("Angrypenguins", "67854"),
+        ("cohort12stars", "876543554");
